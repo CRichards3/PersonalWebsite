8b37780fb6a3562c2d96094c4fe9f9c884b50cfa
Connor <ctricha1@asu.edu>
2019-11-26 23:49:47 -0700

    First commit, initial setup of website

---
+++ src/resumeData.js
@@ -0,0 +1,121 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Connor Richards",
+  role: "Incoming Software Engineer Intern at Microsoft",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "Currently pursuing a Bachelor's degree in Computer Science with a concentration in Software Engineering at Arizona State University. ",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/connor-richards-9a79b1168/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://github.com/CRichards3",
+      className: "fa fa-github"
+    }
+  ],
+  aboutme:
+    "I am currently a Sophomore studying CS at Arizona State University. As of now, I work as a Virtual Reality Research Intern at Meteor Studio. I am also pursuing an online professional certificate through IBM in Data Science!",
+  address: "Tempe",
+  website: "https://github.com/CRichards3",
+  education: [
+    {
+      UniversityName: "Arizona State University (Barrett, the Honors College)",
+      specialization: "Computer Science: Software Engineering",
+      MonthOfPassing: "May",
+      YearOfPassing: "2022",
+      Achievements: "Academic Dean's List"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Microsoft",
+      specialization: "Incoming Software Engineer Intern",
+      MonthOfLeaving: "Summer",
+      YearOfLeaving: "2020",
+      Achievements: "Business Applications Group: Dynamics 365 Cloud Team"
+    },
+    {
+      CompanyName: "Meteor Studio",
+      specialization: "Virtual Reality Research Intern",
+      MonthOfLeaving: "Present",
+      YearOfLeaving: "",
+      Achievements:
+        "Created a Virtual Reality Environment of Mars for scientific research, funded by NASA"
+    },
+    {
+      CompanyName: "State Farm",
+      specialization: "Software Development Intern",
+      MonthOfLeaving: "Summer",
+      YearOfLeaving: "2019",
+      Achievements: "Enterprise Technology: Customer Identity Management Team"
+    },
+    {
+      CompanyName: "State Farm",
+      specialization: "Software Development Intern",
+      MonthOfLeaving: "Summer",
+      YearOfLeaving: "2018",
+      Achievements: "Enterprise Technology: Internal Solutions Development Team"
+    }
+  ],
+  skills: [
+    {
+      skillname: "Java",
+      css: "java"
+    },
+    {
+      skillname: "Python",
+      css: "python"
+    },
+    {
+      skillname: "C#",
+      css: "csharp"
+    },
+    {
+      skillname: "C",
+      css: "c"
+    },
+    {
+      skillname: "C++",
+      css: "cplusplus"
+    }
+  ],
+  portfolio: [
+    {
+      name: "Self Driving Car Simulation",
+      description:
+        "A Python application that simulates a self driving car using Machine Learning, specifically Deep Q Learning. Built using PyTorch with a neural network structure.",
+      imgurl: "images/portfolio/aicar.png"
+    },
+    {
+      name: "MyStocks",
+      description:
+        "A fullstack web app that allows users to search and check stocks real time, using the Intrinio API. Built using Reactjs and Java Spring.",
+      imgurl: "images/portfolio/stock.jpg"
+    },
+    {
+      name: "Biased Media Analyzer",
+      description:
+        "A web application built using Python and Reactjs that uses web scraping across 10+ sites to display news articles and their respective bias ratings.",
+      imgurl: "images/portfolio/news.jpg"
+    },
+    {
+      name: "College Schedule Planner",
+      description:
+        "A Java GUI app that allows user to add courses to a four year course map to plan their collge future. Built with JavaFX and OOP.",
+      imgurl: "images/portfolio/college.jpg"
+    },
+    {
+      name: "BlackJack",
+      description:
+        "A simple Java application that simulates BlackJack, uses essential OOP concepts such as classes and objects.",
+      imgurl: "images/portfolio/blackjack.jpg"
+    }
+  ]
+};
+
+export default resumeData;
